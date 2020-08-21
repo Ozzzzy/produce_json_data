af07e293fbc3bb82fdcdfda480cb9b05a9899794
learningIsInfinite <18333787857@163.com>
2020-08-21 09:53:50 +0800

    master

--- src/main/java/com/example/produce/ProduceApplication.java
+++ src/main/java/com/example/produce/ProduceApplication.java
@@ -1,9 +1,14 @@
 package com.example.produce;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author learningIsInfinite
+ */
 @SpringBootApplication
+@MapperScan("com.example.produce.mapper")
 public class ProduceApplication {
 
     public static void main(String[] args) {
